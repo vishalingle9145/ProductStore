e54f6088445f89b7ca15a9e0f10579b9198a7f63
dac <dac@dypm.org>
2023-10-10 17:45:10 +0530

    Vishal changed something

--- ProductStore/main.cpp
+++ ProductStore/main.cpp
@@ -10,9 +10,15 @@ int main() {
 	
 //	pm.Insert(p);
 	//pm.Delete(1);
+	int choice;
+	do{
+		
+	cout<<"1. InsertData\n 2. DeleteData\n 3. UpdateData\n 4. RetrieveData\n";
 	
+	cout<<"Enter Your choice: ";
+	cin>>choice;
 	
-	cout<<"1. InsertData\n 2. DeleteData\n 3. UpdateData\n 4. RetrieveData"
+}while(choice != 0);
 	
 	
 	return 0;
