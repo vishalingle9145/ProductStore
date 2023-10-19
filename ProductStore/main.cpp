b1b5dafd488b21b3838fcaf69128ab169677b70a
Vishal <you@example.com>
2023-10-19 23:13:46 +0530

    changes by vishal

--- ProductStore/main.cpp
+++ ProductStore/main.cpp
@@ -4,23 +4,25 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	
+
 	Product p;
 //	ProductManager pm;
-	
+
 //	pm.Insert(p);
 	//pm.Delete(1);
 	int choice;
 	do{
-		
+
 	cout<<"1. InsertData\n 2. DeleteData\n 3. UpdateData\n 4. RetrieveData\n";
-	
+
 	cout<<"Enter Your choice: ";
 	cin>>choice;
-	
-	
+
+
+	cout<<"Changes form vishal";
+
 }while(choice != 0);
 	cout<<"Thank You!! visit again";
-	
+
 	return 0;
 }
